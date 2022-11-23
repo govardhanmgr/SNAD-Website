c414963a017365dea247964909bada36d0fa6670
poorna sai <ramisetti.poornasai@gamil.com>
2022-11-23 17:48:25 +0530

    changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CmsservicesService } from './cmsservices.service';
+import { AboutComponent } from './layout/about/about.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    
   ],
-  providers: [],
+  providers: [CmsservicesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
