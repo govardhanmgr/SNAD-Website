be1c1415a0545c2bc6500ce72fa6c15bfebc9870
sripathisri <sripathisri8@gmail.com>
2022-12-02 12:18:36 +0530

    update

--- src/app/layout/contactus/contactus.component.ts
+++ src/app/layout/contactus/contactus.component.ts
@@ -1,16 +1,81 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
-  styleUrls: ['./contactus.component.css']
+  styleUrls: ['./contactus.component.css'],
 })
 export class ContactusComponent implements OnInit {
+  repeatpass: string = 'none';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  registerForm = new FormGroup({
+    firstname: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.pattern('[a-zA-Z].*'),
+    ]),
+    lastname: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.pattern('[a-zA-Z].*'),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    mobile: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9]*'),
+      Validators.minLength(10),
+      Validators.maxLength(10),
+    ]),
+
+    gender: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(15),
+    ]),
+    repearpassword: new FormControl(''),
+  });
+
+  get FirstName(): FormControl {
+    return this.registerForm.get('firstname') as FormControl;
   }
 
+  get LastName(): FormControl {
+    return this.registerForm.get('lastname') as FormControl;
+  }
+
+  get Email(): FormControl {
+    return this.registerForm.get('email') as FormControl;
+  }
+
+  get Mobile(): FormControl {
+    return this.registerForm.get('mobile') as FormControl;
+  }
+
+  get Getgender(): FormControl {
+    return this.registerForm.get('gender') as FormControl;
+  }
+
+  get Password(): FormControl {
+    return this.registerForm.get('password') as FormControl;
+  }
+
+  get Repearpassword(): FormControl {
+    return this.registerForm.get('repearpassword') as FormControl;
+  }
+
+  registerSubmit() {
+    if (this.Password.value == this.Repearpassword.value) {
+      if (this.registerForm.status == 'VALID') {
+        console.log(this.registerForm.value);
+      }
+      this.repeatpass = 'none';
+    } else {
+      this.repeatpass = 'inline';
+    }
+  }
 }
