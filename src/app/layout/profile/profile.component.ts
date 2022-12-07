a51ab2117172268aca9b439e386f8ab3ecb49b3b
samyukthaalakanti-2579 <samyuktha.alakanti@snaddevelopers.com>
2022-12-07 18:55:59 +0530

    about

--- src/app/layout/profile/profile.component.ts
+++ src/app/layout/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Aos from 'aos';
 
 @Component({
   selector: 'app-profile',
@@ -10,6 +11,9 @@ export class ProfileComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    Aos.init({
+      duration: 1200,
+    })
   }
 
 }
