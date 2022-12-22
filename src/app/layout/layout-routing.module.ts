d203b1ac66a8eec09aa5d90ac2d180cb25912418
rajeshseepana <rajeh.seepana@snaddevelopers.com>
2022-12-22 18:26:12 +0530

    up

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -42,7 +42,7 @@ const routes: Routes = [
   { path: 'businessarticles', component: BusinessarticlesComponent },
   { path: "managementarticles", component: ManagementarticlesComponent },
   { path: "financearticles", component: FinancearticlesComponent },
-  { path: "readarticle", component: ReadarticleComponent },
+  { path: "readarticle/:itemid", component: ReadarticleComponent },
   { path: "positions", component: PositionsComponent },
   {path:"serviceheroimage",component:ServiceheroimageComponent},
 
