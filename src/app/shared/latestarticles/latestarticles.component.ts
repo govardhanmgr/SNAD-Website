2e06088b5f5543f832d46db032bf42f26109a1e5
poorna sai <ramisetti.poornasai@gamil.com>
2023-01-10 16:17:30 +0530

    updated

--- src/app/shared/latestarticles/latestarticles.component.ts
+++ src/app/shared/latestarticles/latestarticles.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-latestarticles',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LatestarticlesComponent implements OnInit {
 
+  @Input() latest!:any
   constructor() { }
 
   ngOnInit(): void {
